1633183c88b20af955f2c043487ce7c45ef659e3
warlock921 <caoyu921@163.com>
2017-10-08 22:10:58 +0800

    生成网页模板及静态文件夹

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -1,5 +1,12 @@
+
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 from .models import Post
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	"""docstring for PostAdmin"""
+	list_display = ('title','slug','pub_date')
+
+
+admin.site.register(Post,PostAdmin)
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from django.template.loader import get_template
+from django.shortcuts import redirect
+from django.http import HttpResponse
+from .models import Post
+from datetime import datetime
 
 # Create your views here.
+def homepage(request):
+	""" #old
+	posts = Post.objects.all()
+	post_list = list()
+	for count,post in enumerate(posts):
+		post_list.append("No.{}:".format(str(count))+str(post)+"<hr>")
+		post_list.append("<small>"+str(post.body)+"</small><br><br>")
+	return HttpResponse(post_list)
+	"""
+
+	template = get_template('index.html')
+	posts = Post.objects.all()
+	now = datetime.now()
+	html = template.render(locals())
+	return HttpResponse(html)
+
+def showpost(request,slug):
+	template = get_template('post.html')
+	try:
+		post = Post.objects.get(slug=slug)
+		if post != None:
+			html = template.render(locals())
+			return HttpResponse(html)
+	except Exception as e:
+		return redirect('/')
